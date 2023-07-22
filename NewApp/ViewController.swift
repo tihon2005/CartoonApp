bace63ca405c9dfeeb5fe4baaa299c1d3fde446f
Tsikhon Bazar <103754328+tihon2005@users.noreply.github.com>
2023-07-22 13:20:17 +0300

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Tikhon Bazar on 22.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
