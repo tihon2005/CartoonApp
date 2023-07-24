0f84cf21c9f661bc584c2359eee86e98571102f7
Tsikhon Bazar <103754328+tihon2005@users.noreply.github.com>
2023-07-24 22:25:08 +0300

    Created API Request

--- NewApp/Controllers/Core/RMCharacterViewController.swift
+++ NewApp/Controllers/Core/RMCharacterViewController.swift
@@ -14,6 +14,8 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        
     }
     
 
